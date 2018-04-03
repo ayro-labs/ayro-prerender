6a08f467be1f60519be8a9cb05c68a70648f6057
Sandro Simas <sandro.csimas@gmail.com>
2018-04-03 01:22:22 -0300

    Updating dependencies

---
+++ tasks/publish.js
@@ -0,0 +1,20 @@
+const {publishTask, commands} = require('@ayro/commons');
+const path = require('path');
+const Promise = require('bluebird');
+
+const WORKING_DIR = path.resolve(__dirname, '../');
+
+function lintProject() {
+  return Promise.coroutine(function* () {
+    commands.log('Linting project...');
+    yield commands.exec('npm run lint', WORKING_DIR);
+  })();
+}
+
+// Run this if call directly from command line
+if (require.main === module) {
+  publishTask.withWorkingDir(WORKING_DIR);
+  publishTask.withLintTask(lintProject);
+  publishTask.isDockerProject(true);
+  publishTask.run();
+}
